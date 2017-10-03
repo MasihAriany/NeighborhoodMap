1f46feaeb918055a46d1daf398f7c9c910d166a0
Masih <masih906@gmail.com>
2017-10-03 15:09:07 +0200

    Loading Map
    Up until now, Google Map API loads in the selected Area alongside
    marking the selected locations in that Area .

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var locations = ko.observableArray([
+	{
+		name: '"Friends" Apartment',
+		coords: {lat: 40.7323981, lng: -74.0053062}
+	},
+	{
+		name: 'Empire State Building',
+		coords: {lat: 40.7485405, lng: -73.9858531}
+	},
+	{
+		name: 'Times Square',
+		coords: {lat: 40.758895, lng: -73.9851100}
+	},
+	{
+		name: 'Central Park',
+		coords: {lat: 40.7828647, lng: -73.9675438}
+	}
+	]);
+	
+var mapCenter = {
+	coords: {lat: 40.7582937, lng: -74.0072724}
+};
+
