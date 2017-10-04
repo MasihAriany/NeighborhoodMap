7358763bea1cbbbf8238f0459c09a988dd6050a3
Masih <masih906@gmail.com>
2017-10-04 12:02:33 +0200

    second version
    List of the locations appear in the left slidebar.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-var locations = ko.observableArray([
+function filtering() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("placesList");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var locations = [
 	{
-		name: '"Friends" Apartment',
+		name: "'Friends' Apartment",
 		coords: {lat: 40.7323981, lng: -74.0053062}
 	},
 	{
@@ -14,10 +41,33 @@ var locations = ko.observableArray([
 	{
 		name: 'Central Park',
 		coords: {lat: 40.7828647, lng: -73.9675438}
+	},
+	{
+		name: "St. Patrick's Cathedral",
+		coords: {lat: 40.7584349, lng: -73.976100}
+	},
+	{
+		name: "Broadway Theatre",
+		coords: {lat: 40.7633581, lng: -73.983060}
 	}
-	]);
+	];
 	
 var mapCenter = {
 	coords: {lat: 40.7582937, lng: -74.0072724}
-};
+}
 
+var loc = function(locData){
+	this.name = ko.observable(locData.name);
+}
+
+var viewModel = function() {
+	var self = this;
+	var a = "Hi";
+	this.locs = ko.observableArray([]);
+	
+	locations.forEach(function(place) {
+		self.locs.push( new loc(place) );
+	});
+}
+
+ko.applyBindings(new viewModel());
