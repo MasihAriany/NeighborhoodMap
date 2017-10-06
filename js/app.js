507d0f9cd45cd4a0aa7b286961c0dbd6917d759b
Masih <masih906@gmail.com>
2017-10-06 23:51:52 +0200

    add comments and some refactoring

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-/* ****************    MAP    **********************/
 
-
-var markers = [];
-
+// for filtering the list in the SlideBar itself.
 function filtering() {
     // Declare variables
     var input, filter, ul, li, a, i;
@@ -20,25 +17,6 @@ function filtering() {
             li[i].style.display = "none";
         }
     }
-}
-
-/*
-function streetView() {
-	$(document).ready(function() {
-		$('.places a').click(function(){
-			var map = new google.maps.Map(document.getElementById('map'), {
-				center: fenway,
-				zoom: 14
-			});
-		})
-	})
-}
-*/
-
-function tests(){
-	var b = $('.places').attr('href');
-	console.log(b);
-	
 }
 
 
@@ -102,13 +80,13 @@ function initMap() {
 	//marker.addListener('click', function() {
 	//	infowindow.open(map, marker);
 	//});
-	var pin = function pin(map, name, lat, lng, text) {
+	var pin = function pin(map, name, lat, lng) {
 	
 		var marker;
 		this.name = ko.observable(name);
 		this.lat = ko.observable(lat);
 		this.lng = ko.observable(lng);
-		this.text = ko.observable(text);
+		//this.text = ko.observable(text);
 		this.marker = ko.observable(marker);
 		
 		var infowindow = new google.maps.InfoWindow({
@@ -122,9 +100,20 @@ function initMap() {
 			title: name,
 			map: map
 		});
+		// when marker be clicked, the name of the location will pop up and marker will bounce 3 times.
 		marker.addListener('click', function() {
-          infowindow.open(map, marker);
+			infowindow.open(map, marker);
+			if (marker.getAnimation() !== null) {
+				marker.setAnimation(null);
+			} else {
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+				setTimeout(function() {
+					marker.setAnimation(null);
+				}, 2140); // 2140 for three bounces
+			}
         });
+		
+		//making markers observable in a way so that filtering would work.
 		this.isVisible = ko.observable(false);
 		this.isVisible.subscribe(function(currentState) {
 			if (currentState) {
@@ -135,29 +124,22 @@ function initMap() {
 		});
 
 		this.isVisible(true);
-		markers.push(marker);
-		
-		
 	}
-	
 
 
-
-/* ****************    REST    **********************/
+/* ****************    viewModel   **********************/
 
 	var viewModel = function() {
 		var self = this;
 		this.locs = ko.observableArray([]);
 		self.query = ko.observable('');
-		var i = 0;
 		locations.forEach(function(place) {
-			self.locs.push( new pin(map, place.name, place.coords.lat, place.coords.lng, i) );
-			i++;
-		});
-		//console.log(self.locs()[3].text());
+			self.locs.push( new pin(map, place.name, place.coords.lat, place.coords.lng) );
 
-		
-		self.filterPins = ko.computed(function () {
+		});
+
+		//filtering the markers on the map by searching
+		self.filterMarks = ko.computed(function () {
 			var search  = self.query().toLowerCase();
 			return ko.utils.arrayFilter(self.locs(), function (pin) {
 				var doesMatch = pin.name().toLowerCase().indexOf(search) >= 0;
@@ -166,6 +148,7 @@ function initMap() {
 			});
 		});
 		
+		//this function is called when a location on the navSlideBar would be clicked.
 		self.showStreetView = function(lati, lngi){
 			var pos = {lat: lati, lng: lngi};
 			var panorama = new google.maps.StreetViewPanorama(
@@ -176,9 +159,8 @@ function initMap() {
                 pitch: 10
               }
             });
-			//console.log(self.locs()[0].name());
 			map.setStreetView(panorama);
-			//we don't need to have nav for now.
+			//we don't need to see the nav for now.
 			closeNav();
 		}
 		
